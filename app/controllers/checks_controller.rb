34e5dad2f86ca9e5a3cb8d607e8f398a522ba0fa
brotandgames <brotandgames@gmail.com>
2019-07-16 09:15:51 +0200

    Fat Model, Skinny Controller.

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -28,7 +28,6 @@ class ChecksController < ApplicationController
 
     respond_to do |format|
       if @check.save
-        create_job(@check) if @check.active
         format.html { redirect_to @check, notice: 'Check was successfully created.' }
         format.json { render :show, status: :created, location: @check }
       else
@@ -43,18 +42,6 @@ class ChecksController < ApplicationController
   def update
     respond_to do |format|
       if @check.update(check_params)
-        if @check.saved_change_to_attribute?(:active)
-          if @check.active
-            create_job(@check)
-          else
-            unschedule_job(@check.job)
-            @check.update_columns(next_contact_at: nil, job: nil)
-          end
-        elsif @check.saved_change_to_attribute?(:cron) || @check.saved_change_to_attribute?(:url)
-          Rails.logger.info "ciao-scheduler Check '#{@check.name}' updates to cron or URL triggered job update"
-          unschedule_job(@check.job)
-          create_job(@check)
-        end
         format.html { redirect_to @check, notice: 'Check was successfully updated.' }
         format.json { render :show, status: :ok, location: @check }
       else
@@ -106,48 +93,4 @@ class ChecksController < ApplicationController
     def check_params
       params.require(:check).permit(:name, :cron, :url, :active)
     end
-
-    def create_job(check)
-      job =
-        Rufus::Scheduler.singleton.cron check.cron, :job => true do
-          url = URI.parse(check.url)
-          begin
-            response = Net::HTTP.get_response(url)
-            http_code = response.code
-          rescue *NET_HTTP_ERRORS => e
-            status = e.to_s
-          end
-          status = http_code unless e
-          last_contact_at = Time.current
-          Rails.logger.info "ciao-scheduler Checked '#{url}' at '#{last_contact_at}' and got '#{status}'"
-          status_before = status_after = ""
-          ActiveRecord::Base.connection_pool.with_connection do
-            status_before = check.status
-            check.update_columns(status: status, last_contact_at: last_contact_at, next_contact_at: job.next_times(1).first.to_local_time)
-            status_after = check.status
-          end
-          if status_before != status_after
-            CheckMailer.with(name: check.name, status_before: status_before, status_after: status_after).change_status_mail.deliver
-            Rails.logger.info "ciao-scheduler Sent 'changed_status' notification mail"
-          end
-        end
-      if job
-        Rails.logger.info "ciao-scheduler Created job '#{job.id}'"
-        check.update_columns(job: job.id, next_contact_at: job.next_times(1).first.to_local_time)
-      else
-        Rails.logger.error "ciao-scheduler Could not create job"
-      end
-      return job
-    end
-
-    def unschedule_job(job_id)
-      job = Rufus::Scheduler.singleton.job(job_id)
-      if job
-        job.unschedule
-        Rails.logger.info "ciao-scheduler Unscheduled job '#{job.id}'"
-      else
-        Rails.logger.info "ciao-scheduler Could not unschedule job: '#{job_id}' not found"
-      end
-    end
-
 end
--- app/models/check.rb
+++ app/models/check.rb
@@ -1,4 +1,6 @@
 class Check < ApplicationRecord
+  after_create :create_job, if: :active?
+  after_update :update_routine
 
   validates :name, presence: true
   validates :url, presence: true
@@ -26,5 +28,65 @@ class Check < ApplicationRecord
       0.0
     end
   end
+
+  private
+
+    def create_job
+      job =
+        Rufus::Scheduler.singleton.cron self.cron, :job => true do
+          url = URI.parse(self.url)
+          begin
+            response = Net::HTTP.get_response(url)
+            http_code = response.code
+          rescue *NET_HTTP_ERRORS => e
+            status = e.to_s
+          end
+          status = http_code unless e
+          last_contact_at = Time.current
+          Rails.logger.info "ciao-scheduler Checked '#{url}' at '#{last_contact_at}' and got '#{status}'"
+          status_before = status_after = ""
+          ActiveRecord::Base.connection_pool.with_connection do
+            status_before = self.status
+            self.update_columns(status: status, last_contact_at: last_contact_at, next_contact_at: job.next_times(1).first.to_local_time)
+            status_after = self.status
+          end
+          if status_before != status_after
+            CheckMailer.with(name: self.name, status_before: status_before, status_after: status_after).change_status_mail.deliver
+            Rails.logger.info "ciao-scheduler Sent 'changed_status' notification mail"
+          end
+        end
+      if job
+        Rails.logger.info "ciao-scheduler Created job '#{job.id}'"
+        self.update_columns(job: job.id, next_contact_at: job.next_times(1).first.to_local_time)
+      else
+        Rails.logger.error "ciao-scheduler Could not create job"
+      end
+      return job
+    end
+
+    def unschedule_job
+      job = Rufus::Scheduler.singleton.job(self.job)
+      if job
+        job.unschedule
+        Rails.logger.info "ciao-scheduler Unscheduled job '#{job.id}'"
+      else
+        Rails.logger.info "ciao-scheduler Could not unschedule job: '#{self.job}' not found"
+      end
+    end
+
+    def update_routine
+      if self.saved_change_to_attribute?(:active)
+        if self.active
+          create_job
+        else
+          unschedule_job
+          self.update_columns(next_contact_at: nil, job: nil)
+        end
+      elsif self.saved_change_to_attribute?(:cron) || self.saved_change_to_attribute?(:url)
+        Rails.logger.info "ciao-scheduler Check '#{self.name}' updates to cron or URL triggered job update"
+        unschedule_job
+        create_job
+      end
+    end
 
 end
