16c6213427a13c17e1f117f89cf38d574cd278ce
brotandgames <brotandgames@gmail.com>
2019-07-16 19:28:14 +0200

    Refactor user interface.

--- app/helpers/checks_helper.rb
+++ app/helpers/checks_helper.rb
@@ -1,6 +1,15 @@
 module ChecksHelper
   def class_for_status(status)
-    status == "200" ? "text-green" : "text-red"
+    case status.to_i
+    when 200..299
+      "success"
+    when 300..399
+      "info"
+    when 400..499
+      "warning"
+    else
+      "danger"
+    end
   end
   def class_for_active(active)
     active ? "text-green" : "text-red"
