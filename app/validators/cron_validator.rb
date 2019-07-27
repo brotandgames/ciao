f37532406f78dffefc9c31198d46ee61629f5ab2
Tarek N. Elsamni <tarek.samni@gmail.com>
2019-07-27 12:12:45 +0100

    Rubocop fix: Lint/IneffectiveAccessModifier

--- app/validators/cron_validator.rb
+++ app/validators/cron_validator.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class CronValidator < ActiveModel::EachValidator
+  def self.compliant?(value)
+    cron = ::Fugit::Cron.parse(value)
+    cron.present?
+  rescue StandardError => e
+    Rails.logger.info "CronValidator Exception: #{e}"
+    false
+  end
+
   def validate_each(record, attribute, value)
     return if value.present? && self.class.compliant?(value)
 
@@ -8,15 +16,5 @@ class CronValidator < ActiveModel::EachValidator
       attribute,
       'is not a valid cron. Check your cron schedule expression here: https://crontab.guru'
     )
-  end
-
-  private
-
-  def self.compliant?(value)
-    cron = ::Fugit::Cron.parse(value)
-    cron.present?
-  rescue StandardError => e
-    Rails.logger.info "CronValidator Exception: #{e}"
-    false
   end
 end
--- app/validators/http_url_validator.rb
+++ app/validators/http_url_validator.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 class HttpUrlValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return if value.present? && self.class.compliant?(value)
-
-    record.errors.add(attribute, 'is not a valid HTTP URL')
-  end
-
-  private
-
   def self.compliant?(value)
     escaped_address = URI.escape(value)
     uri = URI.parse(escaped_address)
     uri.is_a?(URI::HTTP) && !uri.host.nil?
   rescue URI::InvalidURIError
     false
+  end
+
+  def validate_each(record, attribute, value)
+    return if value.present? && self.class.compliant?(value)
+
+    record.errors.add(attribute, 'is not a valid HTTP URL')
   end
 end
