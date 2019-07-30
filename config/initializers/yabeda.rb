3914044a890cb21f0bb58eda7ebc427d68a88a5b
brotandgames <brotandgames@gmail.com>
2019-07-30 15:19:17 +0200

    Fix rubocop.

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 prometheus_enabled = ENV.fetch('PROMETHEUS_ENABLED', 'false')
 if prometheus_enabled == 'true'
   Yabeda.configure do
     group :ciao do
-      gauge :checks, comment: "Number of checks"
-      gauge :checks_active, comment: "Number of active checks"
-      gauge :checks_healthy, comment: "Number of healthy checks"
+      gauge :checks, comment: 'Number of checks'
+      gauge :checks_active, comment: 'Number of active checks'
+      gauge :checks_healthy, comment: 'Number of healthy checks'
     end
     # This block will be executed periodically few times in a minute
     # (by timer or external request depending on adapter you're using)
