cd047e7375d88aae534a031415d0a1f66485590d
brotandgames <brotandgames@gmail.com>
2019-08-05 15:45:33 +0200

    Add checks_active_status_*** variables to prometheus exporter.

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -7,6 +7,12 @@ if prometheus_enabled == 'true'
       gauge :checks, comment: 'Number of checks'
       gauge :checks_active, comment: 'Number of active checks'
       gauge :checks_healthy, comment: 'Number of healthy checks'
+      gauge :checks_active_status_1xx, comment: 'Number of active checks with status 1xx'
+      gauge :checks_active_status_2xx, comment: 'Number of active checks with status 2xx'
+      gauge :checks_active_status_3xx, comment: 'Number of active checks with status 3xx'
+      gauge :checks_active_status_4xx, comment: 'Number of active checks with status 4xx'
+      gauge :checks_active_status_5xx, comment: 'Number of active checks with status 5xx'
+      gauge :checks_active_status_err, comment: 'Number of active checks with status err'
     end
     # This block will be executed periodically few times in a minute
     # (by timer or external request depending on adapter you're using)
@@ -15,6 +21,12 @@ if prometheus_enabled == 'true'
       ciao.checks.set({}, Check.count)
       ciao.checks_active.set({}, Check.active.count)
       ciao.checks_healthy.set({}, Check.healthy.count)
+      ciao.checks_active_status_1xx.set({}, Check.status_1xx.count)
+      ciao.checks_active_status_2xx.set({}, Check.status_2xx.count)
+      ciao.checks_active_status_3xx.set({}, Check.status_3xx.count)
+      ciao.checks_active_status_4xx.set({}, Check.status_4xx.count)
+      ciao.checks_active_status_5xx.set({}, Check.status_5xx.count)
+      ciao.checks_active_status_err.set({}, Check.status_err.count)
     end
   end
 end
